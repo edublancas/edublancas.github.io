1ac9bc544a762b32c669dc35fb8f9191980d6b65
Eduardo Blancas <fkq8@blancas.io>
2017-11-30 11:24:13 -0500

    updates cv

--- _resume/build.py
+++ _resume/build.py
@@ -1,14 +1,22 @@
 """
 Build resume and cv from resume.md and cv.md
 """
+import subprocess
 import os.path
 from jinja2 import Environment, FileSystemLoader
 from datetime import date
 
+
+def get_commit_hash():
+    out = subprocess.check_output('git show --oneline -s', shell=True)
+    return out.decode('utf-8') .replace('\n', '').split(' ')[0]
+
+
 path = os.path.dirname(os.path.abspath(__file__))
 env = Environment(loader=FileSystemLoader(path))
 
-now = 'Generated on {}'.format(date.today().strftime('%b, %d, %Y'))
+git_hash = get_commit_hash()
+now = '{}. {}'.format(date.today().strftime('%b %d, %Y'), git_hash)
 
 # this will remove the jinja2 tags
 resume = env.get_template('resume.md').render(referral=True, now=now)
